f2805839830c2bec9e15eaa716c8de75f941b8b9
makanab <makanabrian1@gmail.com>
2020-07-10 13:24:50 +0300

    toggler changes

--- src/nav.js
+++ src/nav.js
@@ -3,12 +3,8 @@ const burger = document.querySelector('.burger');
 const navItems = document.querySelector('.navigation-items');
 
 burger.addEventListener('click',()=>{
-navItems.classList.toggle('.nav-toggler');
+navItems.classList.toggle('nav-toggler');
     console.log('bugger clicked');
-
-    console.log(navItems.classList.toggle('.nav-toggler'));
-    console.log(navItems);
-
 });
 
 }
